30a8f4ebf8364c3d675e21a071ba7933a2cc0a8d
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-09-03 16:05:00 -0400

    idk

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -6,7 +6,7 @@
     <button class="specialButton duration-200 cursor-pointer">
         <p class="text-base sm:text-lg md:text-xl">Get Started</p>
     </button>
-    <button on:click={() => goto('/dashboard')} class="specialButton duration-200= cursor-pointer">
+    <button on:click={() => goto('/dashboard')} class="specialButton duration-200 cursor-pointer">
         <p class="text-base sm:text-lg md:text-xl"> Browse As Guest</p>
     </button>
 </div>
