98fbfa39455fb5f59c23c9dad666b08c4c20ee79
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-09-07 01:45:48 -0400

    feat: added sidebar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,15 +1,21 @@
 <script>
     import { goto } from '$app/navigation';
+    import Sidebar from './Sidebar.svelte';
+    let showSidebar = false;
+
+    const toggleSidebar = () => {
+    showSidebar = !showSidebar;
+  };
 </script>
 
-<header class="flex flex-col relative z-20">
+<header class="flex flex-col relative z-5">
     <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
         <a href="/">
-            <h1 class="text-indigo-400 font-bold text-3xl">Nothing<span class="text-blue-400">.com</span></h1>
+            <h1 class="text-indigo-400 font-bold text-3xl z-5">Nothing<span class="text-blue-400">.com</span></h1>
         </a>
-        <button class="md:hidden grid place-items-center">
+        <button on:click={toggleSidebar} class="md:hidden grid place-items-center">
             <i class="fa-solid fa-bars"></i>
-        </button>
+          </button>
         <nav class="hidden md:flex items-center gap-4 lg:gap-6">
             <a href="#About" class="duration-200 hover:text-indigo-400 cursor-pointer">About The Developers</a>
             <a href="#Reviews" class="duration-200 hover:text-indigo-400 cursor-pointer">Reviews</a>
@@ -21,3 +27,5 @@
     </div>
 
 </header>
+
+<Sidebar {showSidebar} />
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { goto } from '$app/navigation';
+    export let showSidebar = false;
+  </script>
+  
+  <div
+    class="fixed top-0 right-0 w-64 h-full bg-white text-black transform transition-transform duration-300 z-20"
+    style="transform: translateX({showSidebar ? '0' : '100%'})"
+  >
+  <nav class="flex flex-col items-start gap-4 p-4">
+        <a href="#about">About The Developers</a>
+        <a href="#reviews">Reviews</a>
+        <button><a href="#FAQs">FAQs</a></button>
+        <button on:click={() => goto('/login')} class="logInButton mx-auto duration-200cursor-pointer">
+            <p>Log In</p>
+        </button>
+
+    </nav>
+  </div>
+  
+  {#if showSidebar}
+    <div
+      class="fixed inset-0 bg-gray-900 opacity-50 z-10"
+      on:click={() => showSidebar = false}
+    ></div>
+  {/if}
+  
