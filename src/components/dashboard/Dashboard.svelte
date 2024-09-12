c17b6c1399537af59f3af4834db94e5632bdc9e9
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-09-12 11:27:59 -0400

    feat: added loader

--- src/components/dashboard/Dashboard.svelte
+++ src/components/dashboard/Dashboard.svelte
@@ -4,13 +4,17 @@
   import CreatePost from '../../components/dashboard/CreatePost.svelte';
   import PostList from '../../components/dashboard/PostList.svelte';
   import pb from '../../lib/pocketbase.js';
+  import Loader from '../../components/Loader.svelte';
 
   let posts = [];
+  let loading = true;
 
   const fetchPosts = async () => {
+    loading = true;
     const records = await pb.collection('posts').getFullList(); // Fetch posts from PocketBase
     posts = records.map(record => ({ id: record.id, content: record.content }));
     console.log(posts)
+    loading = false;
   };
 
   // Fetch posts when the component mounts
@@ -19,14 +23,20 @@
   });
 
   const addPost = async (newPostContent) => {
+    loading = true;
     await pb.collection('posts').create({ content: newPostContent });
     await fetchPosts(); // Refresh post list
+    loading = false;
   };
 </script>
 
 <Header />
 
 <main class="container mx-auto mt-10">
-  <CreatePost on:createPost={event => addPost(event.detail)} />
-  <PostList {posts} />
+  {#if loading}
+    <Loader />
+  {:else}
+    <CreatePost on:createPost={event => addPost(event.detail)} />
+    <PostList {posts} />
+  {/if}
 </main>
